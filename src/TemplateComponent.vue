573b34b39f95c8b53a8897116ee6e6b0087575e8
Nadia <rebinnaf@yahoo.com>
2021-07-26 11:42:38 +0430

    add template component

---
+++ src/TemplateComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div></div>
+</template>
+<script lang="ts">
+import {
+  Component, Prop, Vue, Watch,
+} from 'vue-property-decorator';
+
+@Component({ name: 'TemplateComponent', components: {} })
+export default class MyComponent extends Vue {
+  @Prop() private msg!: string;
+
+  @Watch('msg')
+  msgChanged(newVal: string) {
+    this.msg = newVal;
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
