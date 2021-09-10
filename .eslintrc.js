04b453ccd3880162dd531a25de2eeacffae4032a
Nadia <rebinnaf@yahoo.com>
2021-09-10 18:12:35 +0430

    change prettier config and reformat some files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
-    "standard",
-    "plugin:unicorn/recommended",
-    "plugin:vue/recommended",
-    "@vue/airbnb",
-    "@vue/typescript",
-    "plugin:prettier-vue/recommended",
-    "prettier",
+    'standard',
+    'plugin:unicorn/recommended',
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+    '@vue/typescript',
+    'plugin:prettier-vue/recommended',
+    'prettier'
   ],
   rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "unicorn/no-nested-ternary": "off",
-    "unicorn/filename-case": "off",
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'unicorn/no-nested-ternary': 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/prefer-module': 'off'
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-  },
-};
+    parser: '@typescript-eslint/parser'
+  }
+}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,7 @@
 declare module '*.vue' {
-  import Vue from 'vue';
+  import Vue from 'vue'
 
-  export default Vue;
+  export default Vue
 }
+
+declare module '*.jpg'
