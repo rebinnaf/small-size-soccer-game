34687b742c79392b94b69f6fca9bccecf18287e2
Nadia <rebinnaf@yahoo.com>
2021-09-11 12:18:11 +0430

    add basic field to the scene

--- src/components/Ground.vue
+++ src/components/Ground.vue
@@ -1,56 +1,40 @@
 <template>
-  <div><div></div></div>
+  <div></div>
 </template>
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
-import * as THREE from "three";
+import * as THREE from 'three'
 
-@Component({ name: "Ground", components: {} })
-export default class MyComponent extends Vue {
-  @Prop() private msg!: string;
+import { generateGroundMaterial } from '../utils/materials'
+import { generateGroundGeometry } from '../utils/geometries'
 
-  @Watch("msg")
-  msgChanged(newValue: string) {
-    this.msg = newValue;
-  }
+@Component({
+  name: 'Ground',
+  components: {}
+})
+export default class Ground extends Vue {
+  @Prop() private scene!: THREE.Scene
 
-  private renderer = new THREE.WebGLRenderer({ alpha: true });
+  @Prop() private frustumSize!: number
 
-  private material = new THREE.MeshNormalMaterial();
+  private material = generateGroundMaterial()
 
-  private camera!: THREE.PerspectiveCamera;
+  private geometry!: THREE.PlaneGeometry
 
-  private scene = new THREE.Scene();
-
-  private geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
-
-  private mesh = new THREE.Mesh(this.geometry, this.material);
+  private mesh!: THREE.Mesh
 
   mounted() {
-    this.msg = "";
-
-    this.camera = new THREE.PerspectiveCamera(
-      100,
-      window.innerWidth / window.innerHeight,
-      0.01,
-      10
-    );
-    this.camera.position.z = 5;
-
-    this.scene.add(this.mesh);
-
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setAnimationLoop(this.animation);
-    document.body.append(this.renderer.domElement);
+    this.initializeGround()
   }
 
-  animation(time) {
-    this.mesh.rotation.x = time / 2000;
-    this.mesh.rotation.y = time / 1000;
+  private initializeGround() {
+    this.geometry = generateGroundGeometry(this.frustumSize / 2)
+    this.material = generateGroundMaterial()
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.mesh.rotation.x = Math.PI * -0.5
 
-    this.renderer.render(this.scene, this.camera);
+    this.scene.add(this.mesh)
   }
 }
 </script>
