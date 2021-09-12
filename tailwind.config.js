7510a76430500d874aa5d875870b3abd8b537e41
Nadia <rebinnaf@yahoo.com>
2021-09-13 02:05:51 +0430

    fix color styles being purged for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  purge: {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    safelist: ['bg-blue-500', 'bg-red-500', 'border-blue-800', 'border-red-800']
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
   },
